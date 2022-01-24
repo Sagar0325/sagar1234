55a01b6c18e26a0c847c5da804376e3bb9abc4f7
Sagar0325 <98327389+Sagar0325@users.noreply.github.com>
2022-01-24 20:37:04 +0530

    Add files via upload

---
+++ cycle1/add.java
@@ -0,0 +1,15 @@
+import java.util.*;
+public class add {
+    public static void main(String args[])
+
+    {
+        int a,b,sum;
+        System.out.println("Enter the numbers: ");
+        Scanner obj=new Scanner(System.in);
+        a=obj.nextInt();
+        b=obj.nextInt();
+        sum=a+b;
+        System.out.println("Sum="+sum);
+    }
+    
+}
