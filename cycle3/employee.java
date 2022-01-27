8679e36828af557ea19fb6160b9fa9c31b9d2c67
Sagar0325 <98327389+Sagar0325@users.noreply.github.com>
2022-01-27 19:19:59 +0530

    Add files via upload

---
+++ cycle3/employee.java
@@ -0,0 +1,34 @@
+import java.util.*;
+public class employee {
+    String name;
+    int empno,emphno;
+    public static void main(String args[])
+    {
+        Scanner sc=new Scanner(System.in);
+        employee e[]=new employee[10];
+        int n;
+        System.out.println("Enter the number of employees:");
+        n=sc.nextInt();
+        System.out.println();
+         for(int i=0;i<n;i++)
+        {
+            e[i]=new employee();
+            System.out.println("Enter the name of employee"+(i+1)+":");
+            e[i].name=sc.next();
+            System.out.println("Enter the empno of employee"+(i+1)+":");
+            e[i].empno=sc.nextInt();
+            System.out.println("Enter the phone number of employee"+(i+1)+":");
+            e[i].emphno=sc.nextInt();
+            System.out.println();
+        } 
+        System.out.println("The details are:\n");
+        for(int i=0;i<n;i++)
+        {
+            System.out.println("Name of employee"+(i+1)+" is:"+e[i].name);
+            System.out.println("Employee number of employee"+(i+1)+" is:"+e[i].empno);
+            System.out.println("Phone number of employee"+(i+1)+" is:"+e[i].emphno);
+            System.out.println();
+        }
+       }
+    
+}
